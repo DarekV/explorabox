d10974697b8c5639282423875216f28c79cfc768
Darek Vassalli <vassallidarek@gmail.com>
2025-05-16 15:58:08 +0200

    core: add default files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,28 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
     '@nuxt/image',
-    '@nuxt/fonts'
-  ]
+    '@nuxt/fonts',
+  ],
+  ssr: false,
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-05-15',
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
---
+++ page/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 class="text-2xl text-red-500">
+      Test vue
+    </h1>
+    <p>this is a test</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
