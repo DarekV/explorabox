9baf172bc6708ea48a207e9b1c238318e9ae2d64
Darek Vassalli <vassallidarek@gmail.com>
2025-05-21 11:38:06 +0200

    test: ci font

--- components/safe-list.vue
+++ components/safe-list.vue
@@ -1,5 +1,5 @@
 <template>
   <div
-    class="hidden bg-primary bg-primary-light bg-primary-dark bg-secondary bg-secondary-light bg-secondary-dark bg-tertiary bg-tertiary-light bg-background bg-background-dark"
+    class="hidden bg-primary bg-primary-light bg-primary-dark bg-secondary bg-secondary-light bg-secondary-dark bg-tertiary bg-tertiary-light bg-background bg-background-dark font-regular font-medium font-bold font-light"
   />
 </template>
